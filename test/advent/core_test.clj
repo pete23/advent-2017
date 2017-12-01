baae41831d7150a1856f0b0fe20b8940fa7be525
Pete Windle <git@pete23.com>
2017-12-01 20:09:19 +0000

    solution to part 1-2

--- test/advent/core_test.clj
+++ test/advent/core_test.clj
@@ -14,3 +14,17 @@
 (deftest advent-1-1-91212129
   (is (= 9 (advent-1-1 "91212129"))))
 
+(deftest advent-1-2-1212
+  (is (= 6 (advent-1-2 "1212"))))
+
+(deftest advent-1-2-1221
+  (is (= 0 (advent-1-2 "1221"))))
+
+(deftest advent-1-2-123425
+  (is (= 4 (advent-1-2 "123425"))))
+
+(deftest advent-1-2-123123
+  (is (= 12 (advent-1-2 "123123"))))
+
+(deftest advent-1-2-12131415
+  (is (= 4 (advent-1-2 "12131415"))))
