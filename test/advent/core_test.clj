80ba08dcf7ba8e780d5d088d0ac854f7be136c12
Pete Windle <git@pete23.com>
2017-12-01 19:39:57 +0000

    solution for 1.1

--- test/advent/core_test.clj
+++ test/advent/core_test.clj
@@ -2,6 +2,15 @@
   (:require [clojure.test :refer :all]
             [advent.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest advent-1-1-1122
+  (is (= 3 (advent-1-1 "1122"))))
+
+(deftest advent-1-1-1111
+  (is (= 4 (advent-1-1 "1111"))))
+
+(deftest advent-1-1-1234
+  (is (= 0 (advent-1-1 "1234"))))
+
+(deftest advent-1-1-91212129
+  (is (= 9 (advent-1-1 "91212129"))))
+
