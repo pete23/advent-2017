11cc27452013b92225eba1b88da1aeb6208ef869
Pete Windle <git@pete23.com>
2017-12-21 12:41:39 +0000

    refactor day 1

--- test/advent/core_test.clj
+++ test/advent/core_test.clj
@@ -2,33 +2,6 @@
   (:require [clojure.test :refer :all]
             [advent.core :refer :all]))
 
-(deftest advent-1-1-1122
-  (is (= 3 (advent-1-1 "1122"))))
-
-(deftest advent-1-1-1111
-  (is (= 4 (advent-1-1 "1111"))))
-
-(deftest advent-1-1-1234
-  (is (= 0 (advent-1-1 "1234"))))
-
-(deftest advent-1-1-91212129
-  (is (= 9 (advent-1-1 "91212129"))))
-
-(deftest advent-1-2-1212
-  (is (= 6 (advent-1-2 "1212"))))
-
-(deftest advent-1-2-1221
-  (is (= 0 (advent-1-2 "1221"))))
-
-(deftest advent-1-2-123425
-  (is (= 4 (advent-1-2 "123425"))))
-
-(deftest advent-1-2-123123
-  (is (= 12 (advent-1-2 "123123"))))
-
-(deftest advent-1-2-12131415
-  (is (= 4 (advent-1-2 "12131415"))))
-
 (deftest advent-2-1-example
   (is (= 18 (advent-2-1 [[5 1 9 5] [7 5 3] [2 4 6 8]]))))
 
