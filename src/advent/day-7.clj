89c5c944c77ae3aa35d0a0bcdbc846f2fd09e479
Pete Windle <git@pete23.com>
2017-12-21 20:37:23 +0000

    day 7 part 1

--- src/advent/day-7.clj
+++ src/advent/day-7.clj
@@ -20,3 +20,29 @@
        (clojure.string/split-lines)
        (map string-to-program)))
 
+(def day-7-test (map string-to-program
+                     ["pbga (66)"
+                      "xhth (57)"
+                      "ebii (61)"
+                      "havc (66)"
+                      "ktlj (57)"
+                      "fwft (72) -> ktlj, cntj, xhth"
+                      "qoyq (66)"
+                      "padx (45) -> pbga, havc, qoyq"
+                      "tknk (41) -> ugml, padx, fwft"
+                      "jptl (61)"
+                      "ugml (68) -> gyxo, ebii, jptl"
+                      "gyxo (61)"
+                      "cntj (57)"]))
+
+; the one at the base cannot be a child
+(defn find-base [programs]
+  (let [all-children (set (mapcat :children programs))]
+      (some #(if (not (all-children %)) %) (map :name programs))))
+
+(deftest find-base-works
+  (is (= "tknk" (find-base day-7-test))))
+
+(defn part-1 []
+  (find-base (read-file-as-list-of-programs "input/day-7.txt")))
+
