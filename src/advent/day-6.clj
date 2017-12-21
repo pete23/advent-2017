08a9e6f646093efbca56177d972634c67c335444
Pete Windle <git@pete23.com>
2017-12-21 12:30:44 +0000

    day 6 part 2 - lazy-seq for the win

--- src/advent/day-6.clj
+++ src/advent/day-6.clj
@@ -1,6 +1,7 @@
 (ns advent.day-6
   (:use clojure.test))
 
+;; return the index of the maximum value
 (defn max-idx
   ([v] (max-idx v 0 -1 Long/MIN_VALUE))
   ([v i max-i max-v]
@@ -12,7 +13,7 @@
 ;; suspect we want to transient this for perf, let's see
 (defn inc-from-i-n-times-wrapping [v i n]
   (if (= n 0) v
-      (let [i (if (= i (count v)) 0 i)] 
+      (let [i (if (= i (count v)) 0 i)] ; wrap if necessary
         (recur (assoc v i (inc (nth v i)))
                (inc i)
                (dec n)))))
@@ -27,17 +28,26 @@
 (deftest check-redistribution
   (is (= [2 4 1 2] (redistribute day-6-test))))
 
-(defn redistributed-result-set
-  ([v] (redistributed-result-set v #{}))
-  ([v s] (if (s v) s
-             (recur (redistribute v) (conj s v)))))
+;; this now includes the redistribution which fails the check to enable part 2
+;; so you need to take one off your count to answer part 1!
+(defn redistributed-result-seq
+  ([v] (redistributed-result-seq v #{}))
+  ([v s] (if (s v) (vector v)
+             (cons v (lazy-seq (redistributed-result-seq (redistribute v) (conj s v)))))))
 
-(deftest check-redistributed-set
-  (is (= 5 (count (redistributed-result-set day-6-test)))))
+(deftest check-redistributed-seq
+  (is (= 5 (dec (count (redistributed-result-seq day-6-test))))))
 
 (def day-6-input [4 10 4 1 8 4 9 14 5 1 14 15 0 15 3 5])
 
 (defn day-6-1 []
-  (count (redistributed-result-set day-6-input)))
+  (dec (count (redistributed-result-seq day-6-input))))
+
+(defn day-6-2 []
+  (let [distributions (redistributed-result-seq day-6-input)
+        loop-value (last distributions)
+        loop-start (some #(if (= loop-value (second %)) (first %))
+                         (map-indexed #(vector %1 %2) distributions))]
+    (- (dec (count distributions)) loop-start)))
 
 
