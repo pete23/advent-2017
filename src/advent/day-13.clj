d4f980982c83ac36dc3010aca61134c632aadeb2
Pete Windle <git@pete23.com>
2017-12-22 21:37:26 +0000

    13 part 2 bruuuuute force and 3 seconds will buy you a coffee

--- src/advent/day-13.clj
+++ src/advent/day-13.clj
@@ -18,10 +18,14 @@
 
 (def test-input {0 3, 1 2, 4 4, 6 4})
 
+(defn failed-layer?
+  ;; if we treat the layer as a modulo round then we hit when rem depth (2range-2) == 0
+  ([[depth range]] (failed-layer? [depth range] 0))
+  ([[depth range] delay] (= 0 (rem (+ depth delay) (- (* 2 range) 2)))))
+
 (defn layer-severity [[depth range]]
   ;; severity is depth * range iff the layer is hit
-  ;; if we treat the layer as a modulo round then we hit when rem depth (2range-2) == 0
-  (if (= 0 (rem depth (- (* 2 range) 2)))
+  (if (failed-layer? [depth range])
     (* depth range)
     0))
 
@@ -32,3 +36,17 @@
   (is (= 24 (total-severity test-input))))
 
 (defn part-1 [] (total-severity input))
+
+
+(defn successful-delay? [firewall delay]
+  (if (not (some #(failed-layer? % delay) firewall)) delay))
+
+(defn delay-to-pass [firewall]
+  (some #(successful-delay? firewall %) (range)))
+
+(deftest part-2-example
+  (is (= 10 (delay-to-pass test-input))))
+
+;; there's probably an analytic solution to this based on the arithmetic
+;; this completes in about a second or three so i'll stop fretting
+(defn part-2 [] (delay-to-pass input))
