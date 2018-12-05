177aac474d3caa5da35c964af51564f01fa9e787
Pete Windle <git@pete23.com>
2018-12-05 12:07:49 +0000

    save state before starting 2018@

--- src/advent/day-23.clj
+++ src/advent/day-23.clj
@@ -21,3 +21,19 @@
   (if (pc-out-of-bounds? program state) state
       (recur program (advent.register-machine/take-step program state))))
 
+(defn part-1
+  ([] (part-1
+       (advent.register-machine/read-file-as-program part-1-instruction-set "input/day-23.txt")
+       part-1-initial-state))
+  ([program state] (:mul-count (run-program program state))))
+
+;; when a=0, b=99, c=99, h=1
+;; when a=0, b=98, c=98, h=1
+;; when a=0, b=98, c=99,
+;; when a=1, b=-109900, c=-92900, h=
+(defn part-2
+  ([] (part-2
+       (advent.register-machine/read-file-as-program part-1-instruction-set "input/day-23.txt")
+       part-1-initial-state))
+  ([program state] (:h (:registers (run-program program state)))))
+  
